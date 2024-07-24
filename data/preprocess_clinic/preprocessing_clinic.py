4be22f468e6703fcc8c168d130384d50dcd9a097
andrey_kirkicha <kirkicha.andrey@mail.ru>
2024-07-24 15:19:11 +0300

    Wroking network, our data

---
+++ data/preprocess_clinic/preprocessing_clinic.py
@@ -0,0 +1,176 @@
+# Given clinical Xma, generate data, including: XLI, M, Sma, SLI, Tr for infering InDuDoNet
+import numpy as np
+import os
+from scipy.interpolate import interp1d, RegularGridInterpolator
+from .utils import get_config
+from .build_gemotry import initialization, imaging_geo
+import PIL
+from PIL import Image
+
+config = get_config('CLINIC_metal/preprocess_clinic/dataset_py_640geo.yaml')
+CTpara = config['CTpara']                       # CT imaging parameters
+
+mask_thre = 2500 / 1000 * 0.192 + 0.192         # taking 2500HU as a thresholding to segment the metal region
+
+param = initialization()
+ray_trafo, FBPOper = imaging_geo(param)         # CT imaging geometry, ray_trafo is fp, FBPoper is fbp
+
+allXma = []
+allXLI = []
+allM = []
+allSma = []
+allSLI = []
+allTr = []
+allaffine = []
+allfilename = []
+
+# process and save all the to-the-tested volumes
+def clinic_input_data(test_path, res_path, mask_path):
+    img_num = 0 
+
+    mat = []
+    spec = []
+    with open("CLINIC_metal/mar_bh_gen/material.txt", "r+") as f:
+        # Reading from a file
+        for line in f:
+            a, b = [float(x) for x in line[:-1].split(' ')]
+            mat.append([a*1000, b])
+    with open("CLINIC_metal/mar_bh_gen/spectra.txt", "r+") as f:
+        # Reading from a file
+        for line in f:
+            a, b = [float(x) for x in line[:-1].split(' ')]
+            spec.append([a, b])
+    mat  = np.array(mat)
+    spec = np.array(spec)
+    mat_interp = RegularGridInterpolator((mat[:,0],), mat[:,1])
+    mat_grid = np.array([mat_interp([x]) for x in list(spec[:,0])])
+
+    for file_name in os.listdir(test_path):
+        file_path = os.path.join(test_path, file_name)
+
+        # for DeepLesion
+        # image = (open_image(file_path, 'float32').astype(np.int32) - 32768).astype(np.float32)
+        # image = image / 1000 * 0.192 + 0.192
+        # image = np.maximum(image, 0)
+
+        # for CLINIC-metal
+        image = open_image(file_path, 'float32')
+
+        M = np.zeros((CTpara['imPixNum'], CTpara['imPixNum']), dtype='float32')
+        Xgt = np.zeros_like(M)
+        Xma = np.zeros_like(M)
+        XLI = np.zeros_like(M)
+        Tr = np.zeros((CTpara['sinogram_size_x'], CTpara['sinogram_size_y']), dtype='float32')
+        Sgt = np.zeros_like(Tr)
+        Sma = np.zeros_like(Tr)
+        SLI = np.zeros_like(Tr)
+
+        mask_num = 0
+
+        for mask_name in os.listdir(mask_path):
+            cur_mask_path = os.path.join(mask_path, mask_name)
+            mask = open_image(cur_mask_path, 'float32')
+
+            # put metal into slice
+            Xgt = np.copy(image)
+            [x, y] = np.where(mask != 0)
+            Xgt[x, y] = mask[x, y]                                      
+            
+            # segment metal region
+            [rowindex, colindex] = np.where(Xgt > mask_thre)
+            M.fill(0.0)
+            M[rowindex, colindex] = 1                 
+            
+            Pmetal_kev = np.asarray(ray_trafo(M))         
+            Tr = Pmetal_kev > 0      
+            
+            # polychromatic radiation
+            Sgt = np.asarray(ray_trafo(Xgt))
+
+            rho = 0.4505
+            total_sum = 0
+            for i in range(mat_grid.shape[0]):
+                total_sum += spec[i, 1]
+                lin = Tr * mat_grid[i] / rho + Sgt
+                Sma += spec[i, 1] * np.exp(-lin)
+            Sma = -np.log(Sma/total_sum)
+            Xma = np.asarray(FBPOper(Sma))
+
+            # to match metal region of gt and ma images
+            [row, col] = np.where(Xma > mask_thre)
+            Xgt[row, col] = Xma[row, col]
+            
+            # linear interpolation
+            SLI = interpolate_projection(Sma, Tr)
+            XLI = np.asarray(FBPOper(SLI))
+
+            # visualization
+            print("\n======================== ...saving... ========================\n")
+            save_as_image(Xma, img_num, mask_num, res_path, 'Xma')
+            save_as_image(M, img_num, mask_num, res_path, 'M')
+            save_as_image(Tr, img_num, mask_num, res_path, 'Tr')
+            save_as_image(Sma, img_num, mask_num, res_path, 'Sma')
+            save_as_image(SLI, img_num, mask_num, res_path, 'SLI')
+            save_as_image(XLI, img_num, mask_num, res_path, 'XLI')
+
+            allXma.append(Xma)
+            allXLI.append(XLI)
+            allM.append(M)
+            allSma.append(Sma)
+            allSLI.append(SLI)
+            allTr.append(Tr)
+            allfilename.append(file_name)
+
+            mask_num += 1
+
+        img_num += 1
+
+    return allXma, allXLI, allM, allSma, allSLI, allTr, allfilename
+
+def open_image(file_path, d_type='float32'):
+    img = np.array(Image.open(file_path), dtype=d_type)      # ndarray
+    return np.array(Image.fromarray(img).resize((CTpara['imPixNum'], CTpara['imPixNum']), PIL.Image.BILINEAR))     # resize image
+
+def save_as_image(array, img_num, mask_num, res_path, name):
+    for_image = array.astype(np.float32)
+    image = Image.fromarray(for_image)
+    
+    cur_path = os.path.join(res_path, name)
+    
+    image.save(os.path.join(cur_path, name + '_img' + str(img_num) + '_mask' + str(mask_num) + '.tif'))
+    print(name + '\t image saved as ' + name + '_img' + str(img_num) + '_mask' + str(mask_num) + '.tif')
+
+def interpolate_projection(proj, metalTrace):
+    # projection linear interpolation
+    # Input:
+    # proj:         uncorrected projection
+    # metalTrace:   metal trace in projection domain (binary image)
+    # Output:
+    # Pinterp:      linear interpolation corrected projection
+    Pinterp = proj.copy()
+    for i in range(Pinterp.shape[0]):
+        mslice = metalTrace[i]
+        pslice = Pinterp[i]
+
+        metalpos = np.nonzero(mslice==1)[0]
+        nonmetalpos = np.nonzero(mslice==0)[0]
+        pnonmetal = pslice[nonmetalpos]
+        pslice[metalpos] = interp1d(nonmetalpos,pnonmetal)(metalpos)
+        Pinterp[i] = pslice
+
+    return Pinterp
+
+if __name__ == '__main__':
+    test_path = 'CLINIC_metal/test'
+    res_path  = 'CLINIC_metal/generated'
+    mask_path = 'CLINIC_metal/mask'
+
+    names = ['M', 'SLI', 'Sma', 'Tr', 'XLI', 'Xma']
+
+    for name in names:
+        current_path = os.path.join(res_path, name)
+        files = os.listdir(current_path)
+        for file in files:
+            os.remove(os.path.join(current_path, file))
+    
+    clinic_input_data(test_path, res_path, mask_path)
---
+++ test_DICDNet.py
@@ -0,0 +1,135 @@
+import os
+import os.path
+import argparse
+import numpy as np
+import torch
+import time
+import h5py
+from utils import utils_image
+from data.preprocess_clinic.preprocessing_clinic import clinic_input_data
+import PIL
+from PIL import Image
+import utils.save_image as save_img
+from dicdnet import DICDNet
+
+parser = argparse.ArgumentParser(description="ACDNet_Test")
+parser.add_argument("--model_dir", type=str, default="pretrained_model/DICDNet_latest.pt", help='path to model file')
+parser.add_argument("--data_path", type=str, default="data/test/", help='path to test data')
+parser.add_argument("--mask_path", type=str, default="data/mask/", help='path to masks')
+parser.add_argument("--use_GPU", type=bool, default=True, help='use GPU or not')
+parser.add_argument("--gpu_id", type=str, default="0", help='GPU id')
+parser.add_argument("--save_path", type=str, default="results/", help='path to testing results')
+parser.add_argument('--num_M', type=int, default=32, help='the number of feature maps')
+parser.add_argument('--num_Q', type=int, default=32, help='the number of channel concatenation')
+parser.add_argument('--T', type=int, default=3, help='the number of ResBlocks in every ProxNet')
+parser.add_argument('--S', type=int, default=10, help='Stage number')
+parser.add_argument('--etaM', type=float, default=1, help='stepsize for updating M')
+parser.add_argument('--etaX', type=float, default=5, help='stepsize for updating X')
+parser.add_argument('--batchSize', type=int, default=1, help='testing input batch size')
+opt = parser.parse_args()
+
+if opt.use_GPU:
+    os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id
+
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+        print("---  new folder...  ---")
+        print("---  " + path + "  ---")
+    else:
+        print("---  There exsits folder " + path + " !  ---")
+
+pred_path = opt.save_path +'/X_mar/'
+mkdir(pred_path)
+
+def normalized(X):
+    maxX = np.max(X)
+    minX = np.min(X)
+    X = (X - minX) / (maxX - minX)
+    return X
+
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print('Total number of parameters: %d' % num_params)
+
+def image_get_minmax():
+    return 0.0, 1.0
+
+def normalize(data, minmax):
+    data_min, data_max = minmax
+    data = np.clip(data, data_min, data_max)
+    data = data * 255.0
+    data = data.astype(np.float32)
+    data = np.expand_dims(np.transpose(np.expand_dims(data, 2), (2, 0, 1)),0)
+    return data
+
+def tohu(X):           # display window as [-175HU, 275HU]
+    CT = (X - 0.192) * 1000 / 0.192
+    CT_win = CT.clamp_(-175, 275)
+    CT_winnorm = (CT_win +175) / (275+175)
+    return CT_winnorm
+
+def test_image(allXma, allXLI, allM, allSma, allSLI, allTr, vol_idx):
+    Xma = allXma[vol_idx]
+    XLI = allXLI[vol_idx]
+    M = allM[vol_idx]
+    Sma = allSma[vol_idx]
+    SLI = allSLI[vol_idx]
+    Tr = allTr[vol_idx]
+
+    Xma = normalize(Xma, image_get_minmax())
+    XLI = normalize(XLI, image_get_minmax())
+
+    Mask = M.astype(np.float32)
+    Mask = np.expand_dims(np.transpose(np.expand_dims(Mask, 2), (2, 0, 1)),0)    
+    non_mask = 1 - Mask
+    
+    return torch.Tensor(Xma).cuda(), torch.Tensor(XLI).cuda(), torch.Tensor(non_mask).cuda()
+
+def main():
+    # Build model
+    print('Loading model ...\n')
+    model = DICDNet(opt).cuda()
+    model.load_state_dict(torch.load(opt.model_dir))
+    model.eval()
+
+    time_test = 0
+    count = 0
+
+    print('\nload data for DICDNet ...')
+    allXma, allXLI, allM, allSma, allSLI, allTr, allfilename = clinic_input_data(opt.data_path, 'data/generated', opt.mask_path)
+    print('testing DICDNet ...')
+   
+    for vol_idx in range(len(allXma)):
+        print("imag_idx:", vol_idx)
+      
+        Xma, XLI, M = test_image(allXma, allXLI, allM, allSma, allSLI, allTr, vol_idx)
+        
+        with torch.no_grad():
+            if opt.use_GPU:
+                torch.cuda.synchronize()
+            start_time = time.time()
+            X0, ListX, ListA = model(Xma, XLI, M)
+            end_time = time.time()
+            dur_time = end_time - start_time
+            time_test += dur_time
+
+        Xoutclip = torch.clamp(ListX[-1] / 255.0, 0, 0.5)
+        Xoutnorm = Xoutclip / 0.5
+        Xouthu = tohu(Xoutclip)
+
+        pre_Xout = Xoutnorm.data.cpu().numpy().squeeze()
+        
+        image = Image.fromarray(pre_Xout)
+        image.save(pred_path + 'pred_' + str(vol_idx) + '.tif')   
+        print('image pred_' + str(vol_idx) + '.tif saved')
+
+        print('Times: ', dur_time)
+        count += 1
+        print(100*'*')
+
+if __name__ == "__main__":
+    main()
